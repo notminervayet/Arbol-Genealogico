f8fd82a6a7f68e59df0f6a1c15b0402d499cb50f
yoel4545 <yoelr086@gmail.com>
2025-11-03 21:32:43 -0600

    Inicio del mapa.cs
    se agrego la inicializacion del formulario mapa.cs con una pequeña prueba de funcionamiento

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Arbol_1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Mapa());
         }
     }
 }
